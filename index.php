a4cda8c32582fb46d32965334cb19857c088834f
dfreker <117466384+dfreker@users.noreply.github.com>
2022-11-04 09:06:11 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,94 @@
+<?php
+
+//some settings
+$random_images = array(
+	'http://icons.iconarchive.com/icons/zairaam/bumpy-planets/256/07-jupiter-icon.png',
+	'http://www.princeton.edu/~willman/planetary_systems/Sol/Saturn/Saturn.gif',
+	'http://www.solstation.com/stars/venus.gif',
+	'https://mars.nasa.gov/images/flckrEclipse-full2.jpg'
+);
+
+$cover_image = 'http://www.androidguys.com/wp-content/uploads/2016/01/summer-sunset-293095.jpg';
+
+//php code here
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+<title>Three29 Test</title>
+<style>
+
+/* http://meyerweb.com/eric/tools/css/reset/ 
+   v2.0 | 20110126
+   License: none (public domain)
+*/
+html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed, 
+figure, figcaption, footer, header, hgroup, 
+menu, nav, output, ruby, section, summary,
+time, mark, audio, video {
+	margin: 0;
+	padding: 0;
+	border: 0;
+	font-size: 100%;
+	font: inherit;
+	vertical-align: baseline;
+}
+/* HTML5 display-role reset for older browsers */
+article, aside, details, figcaption, figure, 
+footer, header, hgroup, menu, nav, section {
+	display: block;
+}
+body {
+	line-height: 1;
+}
+ol, ul {
+	list-style: none;
+}
+blockquote, q {
+	quotes: none;
+}
+blockquote:before, blockquote:after,
+q:before, q:after {
+	content: '';
+	content: none;
+}
+table {
+	border-collapse: collapse;
+	border-spacing: 0;
+}
+</style>
+<style>
+
+/* css here */
+
+</style>
+<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+<script>
+
+//javascript code here
+	
+</script>
+</head>
+<body>
+<div id="wrapper">
+	<div id="div1" class="divitem">
+	</div>
+	<div id="div2" class="divitem">
+	</div>
+	<div id="div3" class="divitem">
+	</div>
+	<div id="div4" class="divitem">
+	</div>
+</div>
+</body>
+</html>
